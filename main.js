240d3ddbd2a5bab1bdd4d77194b4b9344845d6ef
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-04-08 16:01:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const exchangeRates = {
+    USD: 1.0,
+    PKR: 278.12,
+    EUR: 0.92,
+    GBP: 0.79,
+    SAR: 3.75,
+    JPY: 151.84,
+    CHY: 7.23
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "What currency are you converting from?",
+        type: "list",
+        choices: ["USD", "EUR", "PKR", "GBP", "SAR", "JPY", "CHY"]
+    },
+    {
+        name: "to",
+        message: "What currency are you converting to?",
+        type: "list",
+        choices: ["USD", "EUR", "PKR", "GBP", "SAR", "JPY", "CHY"]
+    },
+    {
+        name: "amount",
+        message: "Please enter the amount",
+        type: "number",
+    }
+]);
+if (user_answer.amount != "number") {
+    console.log("Please enter the valid number");
+}
+let fromConverter = exchangeRates[user_answer.from];
+let toConverter = exchangeRates[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromConverter;
+let convertedAmount = baseAmount * toConverter;
+console.log(convertedAmount.toFixed(2));
